357d7837ebc9ec018a0c201c3e42d7bb0768a7fb
AnojanS <asaba091@uottawa.ca>
2019-04-30 17:01:18 -0400

    used queues to complete pathfinder

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,12 +8,62 @@ public class Pathfinder : MonoBehaviour {
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true; // todo make private
+
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     // Use this for initialization
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        Pathfind();
+        //ExploreNeighbours();
+    }
+
+    private void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while(queue.Count > 0)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searching from: " + searchCenter); // todo remove log
+            HaltIfEndFound(searchCenter);
+        }
+
+        print("Finished pathfinding?");
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Searching from end node, therefore stopping"); // todo remove log
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
     }
 
     private void ColorStartAndEnd()
